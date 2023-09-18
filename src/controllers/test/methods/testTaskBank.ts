45c7852bdd3ed633c3830a6b909d06a3e006f431
Igor Lezhnin <r4yson19@gmail.com>
2023-09-19 00:50:35 +0700

    Implement parallel and sequential testing strategy

--- src/controllers/test/methods/testTaskBank.ts
+++ src/controllers/test/methods/testTaskBank.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import {
-  Browser, Builder, By, Capabilities, Key, until,
+  Browser, Builder, By, Key, until,
 } from 'selenium-webdriver';
 
 // TESTING LOGIN USE CASE
@@ -9,7 +9,9 @@ const testCase1 = `
     await driver.findElement(By.id('username')).sendKeys('admin', Key.TAB);
     await driver.findElement(By.id('password')).sendKeys('admin', Key.TAB, Key.ENTER);
     
-    await driver.wait(until.elementLocated(By.xpath('//h2[contains(text(),\\'Классы\\')]')), 3000);
+    await driver.sleep(1000);
+
+    await driver.findElement(By.xpath('//h2[contains(text(),\\'Классы\\')]'));
   };
 `;
 
@@ -18,11 +20,14 @@ const testCase2 = `
   async (driver, Browser, Builder, By, Key, until) => {
     await driver.findElement(By.id('username')).sendKeys('admin', Key.TAB);
     await driver.findElement(By.id('password')).sendKeys('admin', Key.TAB, Key.ENTER);
-    await driver.wait(until.elementLocated(By.xpath('//h2[contains(text(),\\'Классы\\')]')), 3000);
 
-    const checkersLink = await driver.wait(until.elementLocated(By.xpath('//*[contains(text(),\\'Чекеры\\')]')), 3000);
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//h2[contains(text(),\\'Классы\\')]'));
+    
+    const checkersLink = await driver.findElement(By.xpath('//*[contains(text(),\\'Чекеры\\')]'));
     await checkersLink.click();
-    await driver.wait(until.elementLocated(By.xpath('//*[contains(text(),\\'some-checher-name-10\\')]')), 3000);
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//*[contains(text(),\\'some-checher-name-10\\')]'));
   };
 `;
 
@@ -35,30 +40,75 @@ const testCase3 = `
 
 const testCases = [testCase1, testCase2, testCase3];
 
+// PARALLEL TESTING
 export const testTaskBank = async (req: Request, res: Response) => {
-  // const driver = await new Builder().forBrowser(Browser.CHROME).build();
+  const promises = testCases.map((testCase, idx) => new Promise<void>((resolve, reject) => {
+    // const capabilities = Capabilities.chrome();
+    //
+    // const driver = new Builder()
+    //   .usingServer('http://selenium-hub:4444/wd/hub')
+    //   .withCapabilities(capabilities)
+    //   .build();
 
-  const capabilities = Capabilities.chrome();
+    new Builder().forBrowser(Browser.CHROME).build().then((driver) => {
+    // new Promise<void>((r) => { r(); }).then(() => {
+      // driver.get('http://localhost:3000/').then(() => {
+      driver.get('http://project-to-test:8080/').then(() => {
+        const cb = eval(testCase);
 
-  const driver = new Builder()
-    .usingServer('http://selenium-hub:4444/wd/hub')
-    .withCapabilities(capabilities)
-    .build();
+        cb(driver, Browser, Builder, By, Key, until)
+          .then(() => {
+            resolve();
+            driver.quit();
+          })
+          .catch((e: Record<string, any>) => {
+            e.testCase = idx + 1;
 
-  for (const testCase of testCases) {
-    try {
-      await driver.get('http://project-to-test:8080/');
+            reject(e);
+            driver.quit();
+          });
+      });
+    });
+  }));
 
-      const cb = eval(testCase);
-      await cb(driver, Browser, Builder, By, Key, until);
-    } catch (e) {
-      console.log(e);
-
-      await driver.quit();
-      return res.status(500).send(e);
+  const results = await Promise.allSettled(promises);
+  results.forEach((result) => {
+    if (result.status === 'rejected') {
+      return res.status(500).send(result.reason);
     }
-  }
+  });
 
-  await driver.quit();
-  return res.status(200).send({ message: 'OK' });
+  res.status(200).send({ message: 'OK' });
 };
+
+// SEQUENTIAL TESTING
+// export const testTaskBank = async (req: Request, res: Response) => {
+//   // const capabilities = Capabilities.chrome();
+//   // const driver = new Builder()
+//   //   .usingServer('http://selenium-hub:4444/wd/hub')
+//   //   .withCapabilities(capabilities)
+//   //   .build();
+//
+//   const driver = await new Builder().forBrowser(Browser.CHROME).build();
+//
+//   for (let i = 0; i < testCases.length; i += 1) {
+//     const testCase = testCases[i];
+//
+//     try {
+//       // await driver.get('http://project-to-test:8080/');
+//       await driver.get('http://localhost:3000/');
+//
+//       const cb = eval(testCase);
+//       await cb(driver, Browser, Builder, By, Key, until);
+//     } catch (e: any) {
+//       e.testCase = i + 1;
+//
+//       await driver.quit();
+//       return res.status(500).send(e);
+//     }
+//   }
+//
+//   await driver.quit();
+//
+//   res.status(200).send({ message: 'OK' });
+// };
